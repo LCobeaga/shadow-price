920a77988d383a9e13a59a7ec654b2c181b17ab1
Luca Cobeaga <44830532+LCobeaga@users.noreply.github.com>
2018-12-06 19:24:06 -0800

    began to add select fire capabilities

--- firing_code.ino
+++ firing_code.ino
@@ -5,14 +5,12 @@
 #include <JC_Button.h>   //include code libraries for a button with debounce, effectively its like a cited source, and below im writing the evience i gained from reading it
 #include <Servo.h>       //include code libraries for servos, this is PWM and is how we are contrlling the brushless motors
 
-#define PUSH_RET_PIN 8   //sets pusher return switch to pin 8
 #define TRIGGER_PIN 9    //sets trigger limit switch to pin 9
 #define MOTOR_PIN 10     //sets ESC control to pin 10
 #define ACTUATOR_PIN 11  //sets solenoid pusher pin to 11
 
 Servo flywheelESC;                              // create servo object to control the esc 
 Button trigger(TRIGGER_PIN, 25, true, true);    //creates trigger button
-Button pushret(PUSH_RET_PIN, 25, true, true);   //creates pusher return sensor button
 
 void setup() {
   flywheelESC.attach(MOTOR_PIN);     //attaches the servo on pin 10 to the servo object
@@ -24,25 +22,48 @@ void setup() {
   delay(2000);                          //waits to make sure esc took code, here you hear two more beeps, last one very high pitched
 
   trigger.begin();          //initializes button and connected pin
-  pushret.begin();          //same but for pusher return switch
 }                                       
 
 void loop() {
   trigger.read();
-  pushret.read();
+
+  //semiAutoFire();
+  pulseFire();
+}
+/*
+void semiAutoFire () {
+
   if(trigger.wasPressed()){               //checks to see if trigger is pressed 
     flywheelESC.writeMicroseconds(1840);  //gives a maximum speed value when trigger is pressed
+    delay(30);
     digitalWrite(ACTUATOR_PIN,HIGH);      //gives a signal to MOSFET to allow current flow (MOSFETs are like normally closed switches you open with positive current)
-    delay(30);                            //gives signal for 30 milliseconds since solenoids work on change in current, not just curre
+    delay(25);                            //gives signal for 30 milliseconds since solenoids work on change in current, not just curre
     digitalWrite(ACTUATOR_PIN,LOW);       //closes current flow to solenoid
   } 
+
   else{                                  //if trigger not depressed
     flywheelESC.writeMicroseconds(1140); //gives the lowest possible value, or a speed of 0 when no trigger is pressed
-    digitalWrite(ACTUATOR_PIN,LOW);
+    digitalWrite(ACTUATOR_PIN,LOW);      //also turns off solenoid, can never be too safe (solenoid heats up fast)
   }
+}
+*/
+void pulseFire() {
 
-  if(!trigger.isPressed()&&pushret.isPressed()){   //checks to see if trigger is pressed and pusher is in return position 
-    digitalWrite(ACTUATOR_PIN,LOW);                //closes current flow to solenoid
+  if(trigger.wasPressed()){                //checks to see if trigger is pressed 
+    flywheelESC.writeMicroseconds(1840);  //gives a maximum speed value when trigger is pressed
+    delay(10);
+
+    for(int i = 0; i < 3; i++){
+      digitalWrite(ACTUATOR_PIN,HIGH);      //gives a signal to MOSFET to allow current flow (MOSFETs are like normally closed switches you open with positive current)
+      delay(30);                            //gives signal for 30 milliseconds since solenoids work on change in current, not just curre
+      digitalWrite(ACTUATOR_PIN,LOW);       //closes current flow to solenoid
+      delay(100);
+    }
+  } 
+
+  else{                                  //if trigger not depressed
+    flywheelESC.writeMicroseconds(1140); //
+    digitalWrite(ACTUATOR_PIN,LOW);      //also turns off solenoid, can never be too safe (solenoid heats up fast)
   }
 
 }
